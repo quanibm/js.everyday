820c3c67907e2b8795d4a5cb013d7944e5d2fb91
zengibm <zengibm@gmail.com>
2018-06-20 14:10:29 +0800

    factorial

---
+++ 002 factorial.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: nikolas.zengchunhui 
+ * @Date: 2018-06-20 13:57:58 
+ * @Last Modified by: nikolas.zengchunhui
+ * @Last Modified time: 2018-06-20 14:09:16
+ */
+
+// function factorial(num) {
+//     var _num = 1;
+//     for (let i = num; i >= 0; i--) {
+//         _num *= i;
+//     }
+//     return _num;
+// }
+function factorial(num) {
+    var _num = 1;
+    for (let i = 1; i <= num; i++) {
+        _num *= i;
+    }
+    return _num;
+}
+
+console.log(factorial(5));
