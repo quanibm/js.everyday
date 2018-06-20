31869153c71b06b257ff045bf865404d5f2b3b1b
zengibm <zengibm@gmail.com>
2018-06-20 14:11:24 +0800

    阶乘

--- 002 factorial.js
+++ 002 factorial.js
@@ -2,7 +2,7 @@
  * @Author: nikolas.zengchunhui 
  * @Date: 2018-06-20 13:57:58 
  * @Last Modified by: nikolas.zengchunhui
- * @Last Modified time: 2018-06-20 14:09:16
+ * @Last Modified time: 2018-06-20 14:10:51
  */
 
 // function factorial(num) {
